3bd720e17990eaf40c66f744a104f4f9659d88ef
JashinL <mariopoggi46@gmail.com>
2024-09-19 18:17:53 +0200

    Update game.h

--- game.h
+++ game.h
@@ -48,6 +48,7 @@ public:
     void initializeBackroomsLevels();
     void displayBackroomsLevels() const;
     Entity getRandomEntity() const;
+    void addContainedEntity(const Entity& entity);
 };
 
 #endif
