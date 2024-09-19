b5087160e211a0da61419064c1e89abbb961f617
JashinL <mariopoggi46@gmail.com>
2024-09-19 18:13:59 +0200

    Create game_initializer.h

---
+++ game_initializer.h
@@ -0,0 +1,11 @@
+#ifndef GAME_INITIALIZER_H
+#define GAME_INITIALIZER_H
+
+#include "game.h"
+
+class GameInitializer {
+public:
+    static void initializeContainedEntities(Game& game);
+};
+
+#endif
